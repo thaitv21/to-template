3373d88452c4ef9841f7f04760494b49082db2fc
thaitv21 <thaitv210@gmail.com>
2022-02-23 14:36:07 +0700

    Increase version of template

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   githubUrl: 'https://raw.githubusercontent.com/thaitv21/template-service',
   releaseUrl: 'https://github.com/thaitv21/template-service/archive/refs/tags',
-  templateVersion: '1.0.2'
+  templateVersion: '1.0.3'
 }
